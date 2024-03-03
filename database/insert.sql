20ba66f4b1003dbc7d92ded3a8bbd004015fb8e7
ms-poojary <varshinipoojary04@gmail.com>
2024-03-03 23:03:40 +0530

    updated

---
+++ database/insert.sql
@@ -0,0 +1,17 @@
+-- domain table
+
+INSERT INTO `domain`(`DOMAIN_ID`, `DOMAIN_NAME`, `DESCRIPTION`, `IMAGE`) VALUES
+ ('1','Front End Development','Enjoy creating the user interface and experience of websites or applications',
+ 'https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+
+ INSERT INTO `domain` (`DOMAIN_ID`, `DOMAIN_NAME`, `DESCRIPTION`, `IMAGE`) VALUES
+  ('2', 'Backend Development','delve into the intricate realms of server-side programming, to power the dynamic functionality  of websites.', 
+ 'https://media.istockphoto.com/id/1446702855/photo/neon-business-background-financial-stock-market-graphs-diagram-running-line-and-digits-at-the.jpg?s=612x612&w=0&k=20&c=ocfmAaY6gnpGc4_-icA4mCZsiTyBBLvpZVt4_4BVj8Q=');
+ 
+INSERT INTO `domain` (`DOMAIN_ID`, `DOMAIN_NAME`, `DESCRIPTION`, `IMAGE`) VALUES 
+('3', 'Cyber Security', 'The art of creatively exploring and manipulating computer systems and networks to uncover vulnerabilities',
+ 'https://plus.unsplash.com/premium_photo-1677093906053-037cef85dba4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGN5YmVyJTIwc2VjdXJpdHl8ZW58MHx8MHx8fDA%3D');
+
+ INSERT INTO `domain` (`DOMAIN_ID`, `DOMAIN_NAME`, `DESCRIPTION`, `IMAGE`) VALUES
+  ('4', 'Biology', ' the scientific study of living organisms, delving into the wondrous complexities of evolution.',
+  'https://plus.unsplash.com/premium_photo-1684993466316-81ae0b6c96ae?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fGplbGx5JTIwZmlzaHxlbnwwfHwwfHx8MA%3D%3D');
